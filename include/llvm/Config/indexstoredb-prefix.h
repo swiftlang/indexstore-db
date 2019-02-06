d7f347407890ec11224bd35b5dc55005f5b427b6
Ben Langmuir <blangmuir@apple.com>
2019-02-06 13:25:15 -0800

    [llvm] Mangle all llvm symbols to avoid collisions
    Rename all of the llvm symbols so that they will not collide if another
    copy of llvm is linked into the same image. The use of llvm within
    IndexStore is purely an implementation detail. Using a source-level
    rename is a workaround for the lack of symbol visibility controls in
    swiftpm. Ideally we could do this with a combination of
    `-fvisibility=hidden` and `ld -r`.
    
    This allows us to link into sourcekit-lsp statically without colliding
    with symbols in llbuild.

--- include/llvm/Config/indexstoredb-prefix.h
+++ include/llvm/Config/indexstoredb-prefix.h
@@ -1,4 +1,23 @@
+/*===------- llvm/Config/indexstoredb-prefix.h --------------------*- C -*-===*/
+/*                                                                            */
+/*                     The LLVM Compiler Infrastructure                       */
+/*                                                                            */
+/* This file is distributed under the University of Illinois Open Source      */
+/* License. See LICENSE.TXT for details.                                      */
+/*                                                                            */
+/*===----------------------------------------------------------------------===*/
+
 #ifndef INDEXSTOREDB_PREFIX_H
 #define INDEXSTOREDB_PREFIX_H
+
+/* HACK: Rename all of the llvm symbols so that they will not collide if another
+ * copy of llvm is linked into the same image. The use of llvm within IndexStore
+ * is purely an implementation detail. Using a source-level rename is a
+ * workaround for the lack of symbol visibility controls in swiftpm. Ideally we
+ * could do this with a combination of `-fvisibility=hidden` and `ld -r`.
+*/
+
+#define llvm indexstoredb_llvm
+#define LLVMEnablePrettyStackTrace indexstoredb_LLVMEnablePrettyStackTrace
 
 #endif // INDEXSTOREDB_PREFIX_H
