34e4b09a345e72855c9be9c7b7f3ce134ecf903c
Alex Hoppen <ahoppen@apple.com>
2022-08-03 08:10:43 +0200

    Rename `--build-path` to `--scratch-path` in SwiftPM invocation
    `--build-path` has been deprectated in favor of `--scratch-path` in SwiftPM.

--- Utilities/build-script-helper.py
+++ Utilities/build-script-helper.py
@@ -74,7 +74,7 @@ def get_swiftpm_options(args: argparse.Namespace) -> List[str]:
     """
     swiftpm_args = [
         '--package-path', args.package_path,
-        '--build-path', args.build_path,
+        '--scratch-path', args.build_path,
         '--configuration', args.configuration,
     ]
 
